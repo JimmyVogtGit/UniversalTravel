1c7603c6662cf1f1f964114a447663b34ce45bd8
shifumy <ymblandin@gmail.com>
2022-03-18 20:14:17 +0100

    add the javascript code for the submit button in the contact-us page

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu);
+
+const message =
+  "Merci pour votre message. Nous ne manquerons pas de vous contacter très rapidement !";
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+  });
