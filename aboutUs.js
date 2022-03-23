37784e2fb9591b4d55c313366f1b521286292875
pjamard <80617509+pjamard@users.noreply.github.com>
2022-03-23 22:53:39 +0100

    adding some js actions

---
+++ aboutUs.js
@@ -0,0 +1,34 @@
+function changePic1() {
+  document.getElementById("myImg").src = "/Images/Antonio.jpeg";
+
+}
+function changePic2() {
+  document.getElementById("myImg").src = "/Images/Legolas.jpg";
+}
+
+function changePic3() {
+  document.getElementById("myImg2").src = "/Images/JV.jpeg";
+}
+function changePic4() {
+  document.getElementById("myImg2").src = "/Images/Anakin-Skywalker.jpg";
+}
+
+function changePic5() {
+  document.getElementById("myImg3").src = "/Images/pj.jpeg";
+}
+function changePic6() {
+  document.getElementById("myImg3").src = "/Images/Khal-Drogo.jpg";
+}
+
+function changePic7() {
+  document.getElementById("myImg4").src = "/Images/profil-ym.jpg";
+}
+function changePic8() {
+  document.getElementById("myImg4").src = "/Images/Harry-Potter.jpg";
+}
+
+/*sélectionner les 3 div chacune dans une variable (querySelector)
+
+fonction addEventListener qui va se déclencher sur l'action mouseover
+
+(fonction qui va rajouter la classe à la div)
